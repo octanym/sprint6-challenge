253eb35f47a7a36ad046287eba72d169d0ea1b5f
octanym <cmrnmr@gmail.com>
2023-03-26 20:45:09 -0400

    used hooks and fetched data

--- src/components/Character.js
+++ src/components/Character.js
@@ -4,7 +4,17 @@ import axios from axios;
 
 const App = () => {
 
-  
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then((res) => {
+      console.log(res.data)
+    })
+    .catch((err) => {
+      console.log(err, "the requested data was not returned")
+    })
+  }, [])
 
   return (
     <></>
