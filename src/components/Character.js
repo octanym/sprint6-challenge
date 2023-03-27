ca418081b62c593c00ef08795d3fdc7d968065f8
octanym <cmrnmr@gmail.com>
2023-03-26 21:05:50 -0400

    outlined character component

---
+++ src/components/Character.js
@@ -0,0 +1,8 @@
+import { Card, CardBody, CardTitle, CardSubtitle, Col } from 'reactstrap';
+import styled from 'styled-components';
+
+export const CharacterCard = (props) => {
+  return (
+
+  )
+}
