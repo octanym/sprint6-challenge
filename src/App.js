eee8da76f526aaa1672c732ad3ec3524683cf267
octanym <cmrnmr@gmail.com>
2023-03-26 21:26:08 -0400

    app component styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import styled from 'styled-components';
+import { Container, Row } from 'reactstrap';
 import { CharacterCard } from './components/Character';
+
+const Header = styled.div`
+  background: #90bdbf;
+  padding-top: 1%;
+  padding-bottom: 1%;
+  margin-top: 1%;
+  margin-bottom: 1%;
+  border-radius: 75px;
+`;
+
+const HeaderTitle = styled.h1`
+  text-align: center;
+  color: #443e3e;
+  text-shadow: 1px 1px 5px #fff;
+  font-size: 5rem;
+`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -24,18 +42,23 @@ const App = () => {
   }, []);
 
   return (
-    <div className="App">
-      {characters.map((char, index) => (
-        <CharacterCard
-          key={index}
-          characterName={char.name}
-          characterHeight={char.height}
-          characterEyeColor={char.eye_color}
-          characterHairColor={char.hair_color}
-          characterBirthdate={char.birth_year}
-        />
-      ))}
-    </div>
+    <Container>
+      <Header>
+        <HeaderTitle className="Header"> Star Wars Characters</HeaderTitle>
+      </Header>
+      <Row>
+        {characters.map((char, index) => (
+          <CharacterCard
+            key={index}
+            characterName={char.name}
+            characterHeight={char.height}
+            characterEyeColor={char.eye_color}
+            characterHairColor={char.hair_color}
+            characterBirthdate={char.birth_year}
+          />
+        ))}
+      </Row>
+    </Container>
   );
 };
 
